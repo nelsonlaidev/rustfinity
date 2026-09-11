fa9fc0c6290058720778db7191f74a475c868a4b
Nelson Lai <me@nelsonlai.dev>
2026-09-11 13:05:44 +0800

    feat: return-impl-trait

---
+++ return-impl-trait/src/lib.rs
@@ -0,0 +1,19 @@
+// Finish the function
+pub fn filter_starts_with<'a>(
+    input: &'a [String],
+    s: &'a str,
+) -> impl Iterator<Item = &'a String> + 'a {
+    input.iter().filter(move |x| x.starts_with(s))
+}
+
+// Example usage
+pub fn main() {
+    let input = vec![
+        String::from("apple"),
+        String::from("apricot"),
+        String::from("banana"),
+        String::from("cherry"),
+    ];
+    let filtered: Vec<&String> = filter_starts_with(&input, "ap").collect();
+    println!("{:?}", filtered); // Expected output: ["apple", "apricot"]
+}
