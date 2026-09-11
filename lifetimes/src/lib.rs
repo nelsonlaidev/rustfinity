2cfcf955fc595f387ca7d7b661bb6a19ece61e84
Nelson Lai <me@nelsonlai.dev>
2026-09-11 14:53:31 +0800

    feat: lifetimes

---
+++ lifetimes/src/lib.rs
@@ -0,0 +1,23 @@
+pub fn longest<'a>(a: &'a str, b: &'a str) -> &'a str {
+    if a.chars().count() >= b.chars().count() {
+        a
+    } else {
+        b
+    }
+} // Finish the function
+
+// Example usage
+pub fn main() {
+    let s1 = "short";
+    let s2 = "longer string";
+
+    let result = longest(s1, s2);
+    println!("The longest string is: {}", result);
+    assert_eq!(result, "longer string");
+
+    let s3 = "equal";
+    let s4 = "equal";
+    let result = longest(s3, s4);
+    println!("The longest string is: {}", result);
+    assert_eq!(result, "equal");
+}
