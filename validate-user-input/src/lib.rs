7913e21893516e23363fb5b979f4e15f1abcd68d
Nelson Lai <me@nelsonlai.dev>
2026-09-13 17:42:47 +0800

    feat: validate-user-input

---
+++ validate-user-input/src/lib.rs
@@ -0,0 +1,11 @@
+pub fn validate_user(age: i32, email: &str) -> Result<(), String> {
+    if age < 0 || age > 120 {
+        return Err(String::from("Invalid age"));
+    }
+
+    if !email.contains("@") {
+        return Err(String::from("Invalid email"));
+    }
+
+    Ok(())
+}
