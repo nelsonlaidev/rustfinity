fe8703f5e8a8ef727127861877acea4941e25695
Nelson Lai <me@nelsonlai.dev>
2026-09-13 17:11:05 +0800

    feat: determine-number-characteristics

---
+++ determine-number-characteristics/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn describe_number(n: i32) -> String {
+    if n == 0 {
+        return String::from("Zero");
+    } else if n > 0 {
+        if n % 2 == 0 {
+            return String::from("Positive even");
+        } else {
+            return String::from("Positive odd");
+        }
+    } else {
+        if n % 2 == 0 {
+            return String::from("Negative even");
+        } else {
+            return String::from("Negative odd");
+        }
+    }
+}
